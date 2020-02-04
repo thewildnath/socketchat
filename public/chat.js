09a6b119aca3ba3f55c60fea4f053a9317501ded
thewildnath <nathan97wildenberg@gmail.com>
2020-02-04 15:28:53 +0200

    Add preview when other users are writing a message.

--- public/chat.js
+++ public/chat.js
@@ -2,10 +2,11 @@
 var socket = io.connect('http://localhost:4000');
 
 // Query DOM
-var inputHandle  = document.getElementById('inputHandle');
-var inputMessage = document.getElementById('inputMessage');
-var buttonSend   = document.getElementById('buttonSend');
-var output       = document.getElementById('output');
+var inputHandle  = document.getElementById('inputHandle'),
+    inputMessage = document.getElementById('inputMessage'),
+    buttonSend   = document.getElementById('buttonSend'),
+    output       = document.getElementById('output'),
+    feedback     = document.getElementById('feedback');
 
 // Emit event
 
@@ -16,7 +17,16 @@ buttonSend.addEventListener('click', function() {
     });
 });
 
+inputMessage.addEventListener('keypress', function() {
+    socket.emit('typing', inputHandle.value);
+});
+
 // Listen for events
 socket.on('chat', function(data) {
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    feedback.innerHTML = '';
+});
+
+socket.on('typing', function(data) {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
